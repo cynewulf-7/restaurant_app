108f8f350facbe76ea679c96a018c27c166ad201
cynewulf-7 <106197878+cynewulf-7@users.noreply.github.com>
2022-08-18 23:37:15 +0100

    Frontend complete

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 const Featured = () => {
   const [index, setIndex] = useState(0);
   const images = [
-    "/img/featured.png",
-    "/img/featured2.png",
-    "/img/featured3.png",
+    "/img/Featured.jpg",
+    "/img/Featured1.jpg",
+    "/img/Featured2.webp",
   ];
 
   const handleArrow = (direction) =>{
